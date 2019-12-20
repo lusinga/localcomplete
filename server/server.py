98f002b0e9df21ad11947d6737d39c6dadba5bff
lusinga <lusinga@126.com>
2019-12-20 21:38:07 +0800

    update

--- server/server.py
+++ server/server.py
@@ -33,7 +33,7 @@ def single_process_code(text):
     predicted_text = tokenizer.decode([predicted_index])
     # 打印输入结果
     # print(predicted_text)
-    result = (text + predicted_text)  # .strip()
+    result = (text + predicted_text)
     #predicted_text = predicted_text + ""
     print(result)
     print(time.time()-begin)
@@ -42,35 +42,37 @@ def single_process_code(text):
 
 def process_code(text):
     begin = time.time()
-    # 将输入字符串编码
-    indexed_tokens = tokenizer.encode(text)
 
-    # 将读出的索引标记转化成PyTorch向量
-    tokens_tensor = torch.tensor([indexed_tokens])
-
-    # 设置为eval模式，这样就不会执行训练模式下的Dropout过程
-    model.eval()
-
-    # 使用GPU进行加速，诚实地讲速度不太快
-    #tokens_tensor = tokens_tensor.to('cuda')
-
-    past = None
+    predicted_text = text
 
     # 每一个只能补一个token出来，补一句话需要多次，30次是我拍脑袋的
-    for i in range(0, 3):
+    for i in range(0,5):
+
+        # 以上次预测结果作为本次的输入，所谓的自回归
+        indexed_tokens = tokenizer.encode(predicted_text)
+
+        # 将读出的索引标记转化成PyTorch向量
+        tokens_tensor = torch.tensor([indexed_tokens])
+
+        # 设置为eval模式，这样就不会执行训练模式下的Dropout过程
+        model.eval()
+
+        # 使用GPU进行加速，诚实地讲速度不太快
+        #tokens_tensor = tokens_tensor.to('cuda')
+        #model.to('cuda')
 
         # 进行推理
         with torch.no_grad():
-            outputs, past = model(tokens_tensor, past=past)
-            token = torch.argmax(outputs[0, :])
+            outputs = model(tokens_tensor)
+            predictions = outputs[0]
 
-            indexed_tokens += [token.tolist()]
-            tokens_tensor = token.unsqueeze(0)
-
-    predicted_text = tokenizer.decode([indexed_tokens])
+        # 获取预测的下一个子词
+        predicted_index = torch.argmax(predictions[0, -1, :]).item()
+        # 解码成我们都读懂的文本
+        predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
+        # 打印输入结果
+        print(predicted_text)
     print(time.time()-begin)
-    print(predicted_text)
-
     return predicted_text
 
 
